006057ffdce3277350942bbe03bbbb916d82b1fc
kmfb <zhaotianxion@gmail.com>
2021-03-19 05:04:27 +0800

    新建login组件

---
+++ client/src/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { AUTH_URL } from './constants';
+
+export default function Login() {
+  return (
+    <Container
+      className={'d-flex justify-content-center align-items-center'}
+      style={{ height: '100vh' }}
+    >
+      <a href={encodeURI(AUTH_URL)} className={'btn btn-success btn-lg'}>
+        Login With Spotify
+      </a>
+    </Container>
+  );
+}
